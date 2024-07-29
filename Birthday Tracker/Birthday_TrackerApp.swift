dddf1f201885ee3e6166f9e8a46689c7021f4a7a
Allison <allisonfergason@gmail.com>
2024-07-29 15:17:25 -0700

    Initial Commit

---
+++ Birthday Tracker/Birthday_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Birthday_TrackerApp.swift
+//  Birthday Tracker
+//
+//  Created by Allison Fergason on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Birthday_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
