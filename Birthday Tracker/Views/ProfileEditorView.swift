dafcaef5c49a9e4cac50113cc795ad3ec00d0b3c
Edward Zhou <esz7923@nyu.edu>
2024-09-08 14:27:08 -0400

    added profile edit view

--- Birthday Tracker/Views/ProfileEditorView.swift
+++ Birthday Tracker/Views/ProfileEditorView.swift
@@ -8,8 +8,98 @@
 import SwiftUI
 
 struct ProfileEditorView: View {
+    
+    @State var notifEnabled = true
+    @State var name: String = "Curtis Chung"
+    @State var notes: String = "- Very very very short \n- Um, actually"
+    @State var birthday = Date()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack( spacing: 35 ) {
+            HStack {
+                Button {
+                    withAnimation{
+                       
+                    }
+                } label: {
+                    Text("Cancel")
+                }
+                Spacer()
+                Button {
+                    withAnimation{
+                        
+                    }
+                } label: {
+                    Text("Done")
+                }
+            }
+            .padding(.horizontal)
+            VStack( spacing: 35 ) {
+                Circle()
+                    .fill(Color.random)
+                    .frame(width: 256, height: 256)
+                    TextField("Name", text: $name)
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        .padding(.all)
+                        .multilineTextAlignment(.center)
+                        .background(.white)
+                        .autocorrectionDisabled(true)
+                        .overlay(Divider()
+                            .frame(maxWidth: .infinity), alignment: .top)
+                        .overlay(Divider()
+                            .frame(maxWidth: .infinity), alignment: .bottom)
+               
+            }
+            VStack( alignment: .leading, spacing: 10 ) {
+                DatePicker(
+                    "Birthday",
+                    selection: $birthday,
+                    displayedComponents: [.date]
+                )
+                .foregroundStyle(.secondary)
+                .padding(.horizontal)
+                .frame(height: 55)
+                .background(.white)
+                .overlay(Divider()
+                    .frame(maxWidth: .infinity), alignment: .top)
+                .overlay(Divider()
+                    .frame(maxWidth: .infinity), alignment: .bottom)
+                
+                HStack(alignment: .center) {
+                    Toggle("Notifications", isOn: $notifEnabled)
+                        .foregroundStyle(.secondary)
+                }
+                .padding(.horizontal)
+                .frame(height: 55)
+                .background(.white)
+                .overlay(Divider()
+                    .frame(maxWidth: .infinity), alignment: .top)
+                .overlay(Divider()
+                    .frame(maxWidth: .infinity), alignment: .bottom)
+
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("Notes")
+                        .foregroundStyle(.secondary)
+                    TextEditor(text: $notes)
+                        .frame(height: 60)
+                
+                    Spacer()
+                }
+                .padding(.all)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(.white)
+                .overlay(Divider()
+                    .frame(maxWidth: .infinity), alignment: .top)
+                .overlay(Divider()
+                    .frame(maxWidth: .infinity), alignment: .bottom)
+
+                Spacer()
+            }
+            .padding(.vertical)
+            .background(Color(red: 0.95, green: 0.95, blue: 0.95))
+        }
+        .padding(.top)
+        .background(Color(red: 0.95, green: 0.95, blue: 0.95))
     }
 }
 
