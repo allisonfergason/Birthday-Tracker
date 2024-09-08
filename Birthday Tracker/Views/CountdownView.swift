5c5512a97bf014f97f845d30e944af1d2580e6dd
Allison <allisonfergason@gmail.com>
2024-09-08 18:14:52 -0400

    Created baseline UI for countdown view

--- Birthday Tracker/Views/CountdownView.swift
+++ Birthday Tracker/Views/CountdownView.swift
@@ -9,10 +9,32 @@ import SwiftUI
 
 struct CountdownView: View {
     var body: some View {
-        Text("Birthday Tracker")
+        ScrollView(.vertical) {
+            VStack {
+                Spacer()
+                // get variables to count down
+                Text("8")
+                    .font(.custom("SFUIDisplay-Light", size: 120))
+                Text("months")
+                    .font(.title)
+                Spacer()
+                Text("6")
+                    .font(.custom("SFUIDisplay-Light", size: 120))
+                Text("days")
+                    .font(.title)
+                Spacer()
+            }
+            .padding(.vertical)
+            .frame(minWidth: 600, minHeight: 600, alignment: .center)
+            //.background(Color(.blue))
+        }
+        // change title?
+        .navigationTitle("Countdown")
     }
 }
 
 #Preview {
-    CountdownView()
+    NavigationView {
+        CountdownView()
+    }
 }
