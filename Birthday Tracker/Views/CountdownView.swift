4d3f22e67582852c013477c0be62f0a8c81f76ba
Allison <allisonfergason@gmail.com>
2024-10-16 17:04:34 -0400

    small changes to countdown view UI

--- Birthday Tracker/Views/CountdownView.swift
+++ Birthday Tracker/Views/CountdownView.swift
@@ -13,9 +13,14 @@ struct CountdownView: View {
     let birthdayComponents = DateComponents(calendar: .current, month: 9, day: 14)
     var monthsLeft: Int
     var daysLeft: Int
+    let monthsColor: Color
+    let daysColor: Color
+    let name: String
+    let title: String
     
     init(profile: ProfileModel) {
         let today = Date().removeTimeStamp()
+        name = profile.name
         var birthday = profile.birthday
         var earlier = birthday.todayEarlierThan(date: today)
         while (!earlier) {
@@ -25,11 +30,18 @@ struct CountdownView: View {
         monthsLeft = birthday.monthsFrom(from: today);
         let todayPlusMonths = Calendar.current.date(byAdding: .month, value: monthsLeft, to: today)!
         daysLeft = birthday.daysFrom(from: todayPlusMonths)
+        monthsColor = monthsLeft==0 ? Color(#colorLiteral(red: 0.4500938654, green: 0.9813225865, blue: 0.4743030667, alpha: 1)) : Color(#colorLiteral(red: 0, green: 0.5898008943, blue: 1, alpha: 1))
+        daysColor = daysLeft==0 ? Color(#colorLiteral(red: 0.4500938654, green: 0.9813225865, blue: 0.4743030667, alpha: 1)) : Color(#colorLiteral(red: 0, green: 0.5898008943, blue: 1, alpha: 1))
+        title = "Countdown to "+"\(name)"+"'s birthday"
     }
     
     
     var body: some View {
             VStack {
+                Spacer()
+                Text(title)
+                    .font(.title)
+                    .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: 3)
                 Spacer()
                 VStack {
                     Spacer()
@@ -41,8 +53,9 @@ struct CountdownView: View {
                     Spacer()
                 }
                 .frame(width: 300, height: 200, alignment: .center)
-                .background(Color(#colorLiteral(red: 0.4500938654, green: 0.9813225865, blue: 0.4743030667, alpha: 1)))
+                .background(monthsColor)
                 .cornerRadius(40)
+                .shadow(radius: 15)
                 Spacer()
                 VStack {
                     Spacer()
@@ -54,20 +67,20 @@ struct CountdownView: View {
                     Spacer()
                 }
                 .frame(width: 300, height: 200, alignment: .center)
-                .background(Color(#colorLiteral(red: 0, green: 0.5898008943, blue: 1, alpha: 1)))
+                .background(daysColor)
                 .cornerRadius(40)
+                .shadow(radius: 15)
                 Spacer()
                 Spacer()
             }
             .padding(.vertical)
             .frame(minWidth: 600, minHeight: 600, alignment: .center)
-            .navigationTitle("Countdown")
-            //.background(Color(.blue))
+            //.navigationTitle(title)
     }
 }
 
 #Preview {
     NavigationStack {
-        CountdownView(profile: ProfileModel(name: "CC", birthday: getSampleDate(offset: 10)))
+        CountdownView(profile: ProfileModel(name: "CC", birthday: getSampleDate(offset: 40)))
     }
 }
